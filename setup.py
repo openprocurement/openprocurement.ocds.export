72d2100117e510135b4ad7959fb3b9e9238d8b44
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2016-09-09 19:07:57 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+DESCRIPTION = """
+    Libraty which provides interfaces for storing
+    OCDS release packages.
+"""
+
+install_requires = [
+]
+
+test_requires = [
+]
+
+
+setup(name='ocds.storage',
+      version='0.0.1',
+      description=DESCRIPTION,
+      author='Quintagroup, Ltd.',
+      author_email='info@quintagroup.com',
+      license='Apache License 2.0',
+      include_package_data=True,
+      packages=['ocds.storage'],
+      zip_safe=False,
+      install_requires=install_requires,
+      tests_require=test_requires,
+      )
