8946ff2293d7a1e9612725a21621cd7f593e2c7a
dimon.obert <dimon.obert@quintagroup.org>
2016-09-29 12:40:15 +0300

    added tests for unique documents and None fields

--- tests/test_release.py
+++ tests/test_release.py
@@ -3,10 +3,21 @@ from ocds.export.release import get_release_from_tender
 
 
 def test_tender_release():
-    tender = ((get_test_data()))
+    tender = get_test_data()
+    assert tender['bids']
+    assert tender['submissionMethod']
+    del tender['submissionMethod']
+    assert tender['minimalStep']
+    del tender['minimalStep']
     release = get_release_from_tender(tender, 'sd')
     if tender['status'] not in ['complete', 'unsuccessful', 'cancelled']:
         assert release['tender']['status'] == 'active'
-    assert (release['tender']['numberOfTenderers']) == len(release['tender']['tenderers'])
-    _ids = [i['identifier']['id'] for i in release['tender']['tenderers']]
-    assert len(set(_ids)) == len(_ids)
+    assert (release['tender']['numberOfTenderers']) == len(release[ 'tender']['tenderers'])
+    tenderers_ids = [i['identifier']['id'] for i in release['tender']['tenderers']]
+    assert len(set(tenderers_ids)) == len(tenderers_ids)
+    tender_doc_ids = [i['id'] for i in release['tender']['documents']]
+    assert len(set(tender_doc_ids)) == len(tender_doc_ids)
+    award_doc_ids = [k['id'] for i in release['awards'] for k in i['documents']]
+    contract_doc_ids = [k['id'] for i in release['contracts'] for k in i['documents']]
+    assert len(set(award_doc_ids)) == len(award_doc_ids)
+    assert len(set(contract_doc_ids)) == len(contract_doc_ids)
