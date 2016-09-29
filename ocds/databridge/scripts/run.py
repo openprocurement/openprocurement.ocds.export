62329d12f3ee1ac896002db009fb3c487afa2407
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2016-09-29 16:54:41 +0300

    Use worker

--- ocds/databridge/scripts/run.py
+++ ocds/databridge/scripts/run.py
@@ -3,11 +3,19 @@ import os.path
 import sys
 import yaml
 import logging
+import functools
+from ocds.storage import CouchStorage
+from ..contrib.client import APIClient
 from logging.config import dictConfig
 from ..bridge import APIDataBridge
+from ..helpers import (
+    exists_or_modified,
+    fetch_tenders,
+    save_items
+)
 
 
-LOGGER = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 logging.getLogger("requests").setLevel(logging.WARNING)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 
@@ -27,5 +35,19 @@ def run():
         dictConfig(config['logging'])
     else:
         logging.basicConfig(level=logging.DEBUG)
-    bridge = APIDataBridge(config)
+
+    storage = CouchStorage(config['db'])
+    client = APIClient(
+        config['api']['api_key'],
+        config['api']['api_host'],
+        config['api']['api_version']
+    )
+
+    _filter = functools.partial(exists_or_modified, storage)
+    _fetch = functools.partial(fetch_tenders, client)
+    _save = functools.partial(save_items, storage)
+
+    bridge = APIDataBridge(config, filter_feed=_filter)
+    bridge.add_worker(_fetch)
+    bridge.add_worker(_save)
     bridge.run()
