cc565b6a3ec17d9d3b5e4571a35e8c13135abab7
Yaroslav Shalenyk <yshalenyk@gmail.com>
2016-12-16 01:07:04 +0200

    start new architecture

--- ocds/storage/__init__.py
+++
@@ -1,5 +0,0 @@
-from .backends.couch import TendersStorage, ReleasesStorage
-from .backends.fs import FSStorage
-from .backends.main import MainStorage
-
-__all__ = [TendersStorage, ReleasesStorage, FSStorage, MainStorage]
--- ocds/storage/models.py
+++ ocds/storage/models.py
@@ -1,8 +1,1 @@
-from couchdb.mapping import Document, TextField, BooleanField
 
-
-class ReleaseDoc(Document):
-    path = TextField()
-    finished = BooleanField()
-    _id = TextField()
-    date = TextField()
