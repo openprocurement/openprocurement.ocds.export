d11966fe9a872f06d0048b400f4c324fd0f54195
Yaroslav Shalenyk <yshalenyk@gmail.com>
2016-12-22 00:39:20 +0200

    Create database if not exists

--- openprocurement/ocds/export/storage.py
+++ openprocurement/ocds/export/storage.py
@@ -1,24 +1,62 @@
 import itertools
-from couchdb import Database
+from couchdb import Database, http
 from uuid import uuid4
 from couchdb.design import ViewDefinition
 from openprocurement.ocds.export.models import Release, ReleasePackage, get_ocid, Tender
 from couchdb_schematics.document import Document
 
 
-_releases_ocid = ViewDefinition('releases', 'ocid', map_fun="function(doc) { emit(doc.ocid, doc._id); }", reduce_fun="function(key, values, rereduce) { var k = key[0][0]; var result = result || {}; result[k] = result[k] || []; result[k].push(values); return result; }")
-_releases_all = ViewDefinition('releases', 'all', map_fun="function(doc) { emit(doc._id, doc); }")
-_releases_tag = ViewDefinition('releases', 'tag', map_fun="function(doc) { emit(doc._id, doc.tag); }")
-_tenders_all = ViewDefinition('tenders', 'all', map_fun="function(doc) { if(doc.doc_type !== 'Tender') {return;}; if(doc.status.indexOf('draft') !== -1) {return;}; emit(doc._id, doc); }")
-_tenders_dateModified = ViewDefinition('tenders', 'byDateModified', map_fun="function(doc) { if(doc.doc_type !== 'Tender') {return;}; emit(doc.dateModified, doc); }")
+_releases_ocid = ViewDefinition('releases', 'ocid', 
+    map_fun="""function(doc) { 
+                 emit(doc.ocid, doc._id);
+    }""",
+    reduce_fun="""function(key, values, rereduce) { 
+                        var result = result || [];
+                        result.push.apply(result, values);
+                        return result; 
+    }"""
+)
+
+_releases_all = ViewDefinition('releases', 'all', 
+    map_fun="""function(doc) { 
+                emit(doc._id, doc); 
+    }"""
+)
+_releases_tag = ViewDefinition('releases', 'tag', 
+    map_fun="""function(doc) {
+                    emit(doc._id, doc.tag); 
+    }"""
+)
+_tenders_all = ViewDefinition('tenders', 'all', 
+    map_fun="""function(doc) { 
+                    if(doc.doc_type !== 'Tender') {return;}; 
+                    if(doc.status.indexOf('draft') !== -1) {return;}; 
+                    emit(doc._id, doc); 
+    }"""
+)
+_tenders_date_modified = ViewDefinition('tenders', 'by_dateModified', 
+    map_fun="""function(doc) {
+                if(doc.doc_type !== 'Tender') {return;};
+                emit(doc.dateModified, doc);
+    }"""
+)
+
+def get_or_create(url, name):
+    resource = http.Resource(url, session=None)
+    try:
+        return resource.head(name)
+    except http.ResourceNotFound:
+        resource.put_json(name)
+
 
 
 class TendersStorage(Database):
 
     def __init__(self, db_url, name=None):
         url = "{}/{}".format(db_url, name)
+        get_or_create(db_url, name)
         super(TendersStorage, self).__init__(url=url)
-        ViewDefinition.sync_many(self, [_tenders_all, _tenders_dateModified])
+        ViewDefinition.sync_many(self, [_tenders_all, _tenders_date_modified])
 
     def __iter__(self):
         for item in self.iterview('tenders/all', 1000):
@@ -29,6 +67,7 @@ class ReleasesStorage(Database):
 
     def __init__(self, db_url, name=None):
         url = "{}/{}".format(db_url, name or 'releases')
+        get_or_create(db_url, name)
         super(ReleasesStorage, self).__init__(url=url)
         ViewDefinition.sync_many(self, [_releases_ocid, _releases_all, _releases_tag])
 
