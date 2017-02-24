4387f756b8d0d7dec5568d2aa16a0e68339143a3
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2017-02-24 13:12:26 +0200

    Update html generation; code clean up

--- openprocurement/ocds/export/scripts/packages.py
+++ openprocurement/ocds/export/scripts/packages.py
@@ -1,51 +1,44 @@
 # -*- coding: utf-8 -*-
 import argparse
-import yaml
-import iso8601
 import os
 import logging
 import math
-import zipfile
 import couchdb.json
 import multiprocessing as mp
 from functools import partial
-from itertools import izip_longest
-from logging.config import dictConfig
+from itertools import izip_longest as zip_longest
 from simplejson import dump
-from jinja2 import Environment, PackageLoader
-from openprocurement.ocds.export.storage import TendersStorage
-from boto.s3 import connect_to_region
-from openprocurement.ocds.export.models import package_tenders
-from boto.s3.connection import OrdinaryCallingFormat, S3ResponseError
+from jinja2 import (
+    Environment,
+    PackageLoader
+)
 from filechunkio import FileChunkIO
 
+from openprocurement.ocds.export.helpers import (
+    connect_bucket,
+    read_config,
+    parse_dates,
+    make_zip,
+    dump_json,
+    file_size,
+    get_torrent_link
+)
+from openprocurement.ocds.export.storage import (
+    TendersStorage
+)
+from openprocurement.ocds.export.models import (
+    package_tenders
+)
+
+from openprocurement.ocds.export.ext.models import (
+    package_tenders_ext
+)
 
 couchdb.json.use('simplejson')
-Logger = logging.getLogger(__name__)
-ENV = Environment(loader=PackageLoader('openprocurement.ocds.export',
-                                       'templates'))
-logging.getLogger('boto').setLevel(logging.ERROR)
-
-
-def connect_bucket(config):
-    try:
-        conn = connect_to_region(
-                    'eu-west-1',
-                    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID', ''),
-                    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY', ''),
-                    calling_format=OrdinaryCallingFormat()
-                    )
-        return conn.get_bucket(config.get('bucket'))
-    except S3ResponseError as e:
-        Logger.warn('Unable to connect to s3. Error: {}'.format(e))
-        return False
-
-
-def read_config(path):
-    with open(path) as cfg:
-        config = yaml.load(cfg)
-    dictConfig(config.get('logging', ''))
-    return config
+logger = logging.getLogger('packer')
+ENV = Environment(
+    loader=PackageLoader('openprocurement.ocds.export', 'templates')
+)
 
 
 def parse_args():
@@ -69,25 +62,13 @@ def parse_args():
     return parser.parse_args()
 
 
-def parse_dates(dates):
-    return (iso8601.parse_date(dates[0]).isoformat(),
-            iso8601.parse_date(dates[1]).isoformat())
-
-
-def make_zip(name, base_dir, skip=[]):
-    skip.append(name)
-    with zipfile.ZipFile(os.path.join(base_dir, name),
-                         'w', zipfile.ZIP_DEFLATED, allowZip64=True) as zf:
-        for f in [f for f in os.listdir(base_dir) if f not in skip]:
-            zf.write(os.path.join(base_dir, f))
-
-
 def fetch_and_dump(config, max_date, params, extensions=False):
     nth, (start, end) = params
-    Logger.info('Start {}th dump startdoc={}'
+    logger.info('Start {}th dump startdoc={}'
                 ' enddoc={}'.format(nth, start, end))
     db = TendersStorage(config['tenders_db']['url'],
                         config['tenders_db']['name'])
+    bucket = config.get('bucket')
     if not start and not end:
         return
     if end:
@@ -100,33 +81,40 @@ def fetch_and_dump(config, max_date, params, extensions=False):
                                               startkey=start,
                                               include_docs=True))]
     name = 'release-{0:07d}.json'.format(nth)
+    path = config['path']
     try:
         if extensions:
-            from openprocurement.ocds.export.ext.models import package_tenders_ext
             package = package_tenders_ext(result[:-1], config.get('release'))
-            package['uri'] = 'http://ocds.prozorro.openprocurement.io/merged_with_extensions_{}/{}'.format(max_date, name)
+            package['uri'] = 'http://{}/merged_with_extensions_{}/{}'.format(
+                bucket, max_date, name
+            )
         else:
             package = package_tenders(result[:-1], config.get('release'))
-            package['uri'] = 'http://ocds.prozorro.openprocurement.io/merged_{}/{}'.format(max_date, name)
+            package['uri'] = 'http://{}/merged_{}/{}'.format(
+                bucket, max_date, name
+            )
         date = max(map(lambda x: x.get('date', ''), package['releases']))
     except Exception as e:
-        Logger.info('Error: {}'.format(e))
+        logger.info('Error: {}'.format(e))
         return
-    path = os.path.join(config['path'], 'release-{0:07d}.json'.format(nth))
     if nth == 1:
         if extensions:
-            pretty_package = package_tenders_ext(result[:24], config.get('release'))
-            pretty_package['uri'] = 'http://ocds.prozorro.openprocurement.io/merged_with_extensions_{}/example.json'.format(max_date)
+            pretty_package = package_tenders_ext(
+                result[:24], config.get('release')
+            )
+            pretty_package['uri'] = 'http://{}/merged_with_extensions_{}/'\
+                                    'example.json'.format(bucket, max_date)
         else:
-            pretty_package = package_tenders(result[:24], config.get('release'))
-            pretty_package['uri'] = 'http://ocds.prozorro.openprocurement.io/merged_{}/example.json'.format(max_date)
-        Logger.info('Dump example.json')
-        with open(os.path.join(config['path'], 'example.json'), 'w')\
-                as outfile:
-            dump(pretty_package, outfile, indent=4)
-    with open(path, 'w') as outfile:
-        dump(package, outfile)
-    Logger.info('End {}th dump startdoc={} enddoc={}'.format(nth, start, end))
+            pretty_package = package_tenders(
+                result[:24], config.get('release')
+            )
+            pretty_package['uri'] = 'http://{}/merged_{}/'\
+                                    'example.json'.format(bucket, max_date)
+        logger.info('Dump example.json')
+        dump_json(config['path'], 'example.json',
+                  pretty_package, pretty=True)
+    dump_json(path, name, package)
+    logger.info('End {}th dump startdoc={} enddoc={}'.format(nth, start, end))
     return date
 
 
@@ -155,52 +143,46 @@ def put_to_s3(bucket, path, time, extensions=False):
             key.set_contents_from_filename(file_path)
 
 
-def file_size(path, name):
-    return (os.stat(os.path.join(path, name)).st_size) / 1000000
-
-
-def get_torrent_link(bucket, path):
-    return 'https://s3-eu-west-1.amazonaws.com/'\
-            '{}/{}/releases.zip?torrent'.format(bucket, path)
+def links(path, skip=['example.json', 'index.html', 'releases.zip']):
+    for _file in sorted([f for f in os.listdir(path) if
+                        f not in skip]):
+        yield {
+            'size': file_size(path, _file),
+            'link': _file
+        }
 
 
 def create_html(path, config, date, extensions=False):
     template = ENV.get_template('index.html')
-    links = []
-    skip_files = ['example.json', 'index.html', 'releases.zip']
-    for file in sorted([f for f in os.listdir(path) if
-                        f not in skip_files]):
-        link = {}
-        link['size'] = file_size(path, file)
-        link['link'] = file
-        links.append(link)
-    if extensions:
-        torrent_link = get_torrent_link(config.get('bucket'),
-                                        'merged_{}'.format(date))
-    else:
-        torrent_link = get_torrent_link(config.get('bucket'),
-                        'merged_with_extensions{}'.format(date))
+    key = 'merged_{}' if not extensions else 'merged_with_extensions{}'
+    torrent_link = get_torrent_link(config.get('bucket'), key.format(date))
     zip_size = file_size(path, 'releases.zip')
     with open(os.path.join(path, 'index.html'), 'w') as stream:
         stream.write(template.render(dict(zip_size=zip_size,
                                           torrent_link=torrent_link,
-                                          links=links)))
+                                          links=links(path))))
 
 
-def update_index(bucket, time, extensions=False):
-    key = bucket.new_key('index.html')
-    key.get_contents_to_filename('index.html')
-    if extensions:
-        dir_name = 'merged_with_extensions_{}'.format(time)
-    else:
-        dir_name = 'merged_{}'.format(time)
-    with open('index.html', 'r+') as f:
-        lines = f.readlines()
-    lines.insert(lines.index('</ol></body>\n'),
-                 "<li><a href='{}'>{}</a></li>\n".format(dir_name, dir_name))
-    with open('index.html', 'w') as f:
-        f.write(''.join(lines))
-    key.set_contents_from_filename('index.html')
+def update_index(bucket):
+    template = ENV.get_template('base.html')
+    index = ENV.get_template('index.html')
+    dirs = [d.name for d in bucket.list('merged', '/')]
+    html = template.render(dict(links=[x.strip('/') for x in dirs]))
+    bucket.get_key('index.html').set_contents_from_string(html)
+    logger.info('Updated base index')
+    for path in dirs:
+        ctx = [p.name for p in bucket.list(path, '/')
+               if not p.name.endswith('html')]
+        archive = bucket.get_key(path+'releases.zip')
+        size = None
+        if archive:
+            size = archive.size/1024/1024
+        torrent_link = get_torrent_link(bucket.name, path)
+        files = sorted([{'link': f.split('/')[1], 'size': bucket.get_key(f).size/1024/1024} for f in ctx],
+                       key=lambda x: x.get('link'))
+        result = index.render(dict(zip_size=size, torrent_link=torrent_link, links=files))
+        bucket.get_key(os.path.join(path, 'index.html')).set_contents_from_string(result)
+        logger.info('Updated index in {}'.format(path))
 
 
 def fetch_ids(db, batch_count):
@@ -213,30 +195,35 @@ def run():
     bucket = connect_bucket(config)
     _tenders = TendersStorage(config['tenders_db']['url'],
                               config['tenders_db']['name'])
-    Logger.info('Start packaging')
+    logger.info('Start packaging')
     if not os.path.exists(config.get('path')):
         os.makedirs(config.get('path'))
     if args.dates:
         datestart, datefinish = parse_dates(args.dates)
-        pack = package_tenders(list(_tenders.get_between_dates(datestart, datefinish)), config)
-        with open(os.path.join(config['path'],
-          'release_between_{}_{}'.format(datestart.split('T')[0],
-          datefinish).split('T')[0]), 'w') as stream:
+        to_release = _tenders.get_between_dates(datestart, datefinish)
+        pack = package_tenders(list(to_release), config)
+        name = 'release_between_{}_{}'.format(datestart.split('T')[0],
+                                              datefinish.split('T')[0])
+        with open(os.path.join(config['path'], name, 'w')) as stream:
             dump(pack, stream)
     else:
         max_date = list(_tenders.get_max_date())[-1].split('T')[0]
         total = int(args.number) if args.number else 4096
         key_ids = fetch_ids(_tenders, total)
-        Logger.info('Fetched key doc ids')
-        pool = mp.Pool(mp.cpu_count()*2)
+        logger.info('Fetched key doc ids')
+        pool = mp.Pool(mp.cpu_count())
         if args.ext:
             _conn = partial(fetch_and_dump, config, max_date, extensions=True)
         else:
             _conn = partial(fetch_and_dump, config, max_date)
-        pool.map(_conn, enumerate(izip_longest(key_ids, key_ids[1::],
-                                                           fillvalue=''), 1))
+        params = enumerate(
+            zip_longest(key_ids, key_ids[1::], fillvalue=''),
+            1
+        )
+        pool.map(_conn, params)
         make_zip('releases.zip', config.get('path'))
         create_html(config.get('path'), config, max_date, extensions=args.ext)
         if args.s3 and bucket:
-            put_to_s3(bucket, config.get('path'), max_date, extensions=args.ext)
-            update_index(bucket, max_date, extensions=args.ext)
+            put_to_s3(bucket, config.get('path'),
+                      max_date, extensions=args.ext)
+            update_index(bucket)
